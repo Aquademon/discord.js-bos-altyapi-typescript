fec046b7d617b279aa6445be535b67e7e0a5d528
Aquademon <98233916+Aquademon@users.noreply.github.com>
2024-07-06 13:42:35 +0300

    update src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Client, GatewayIntentBits, Partials, Collection } from "discord.js";
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ],
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction]
+});
+
+client.login(process.env.DISCORD_TOKEN);
