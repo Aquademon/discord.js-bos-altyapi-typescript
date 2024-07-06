8a813b040de2249f94f7b47d5d7825a4da840b1c
Aquademon <98233916+Aquademon@users.noreply.github.com>
2024-07-06 16:06:42 +0300

    update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export default {
     PREFIX: "!",
     MESSAGES: {
-      COOLDOWN_MESSAGE: "Bu komutu kullanabilmek için <duration> beklemelisin." // Mesaj içerisinde <duration> kısmı geriye kalan süreyi gösterir
+        COOLDOWN_MESSAGE:
+            "Bu komutu kullanabilmek için <duration> beklemelisin." // Mesaj içerisinde <duration> kısmı geriye kalan süreyi gösterir
     }
 };
