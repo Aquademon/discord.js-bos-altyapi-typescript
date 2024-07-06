453d4b07be7ad59710c1ac515a1e09b506273887
Aquademon <98233916+Aquademon@users.noreply.github.com>
2024-07-06 16:01:12 +0300

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { bold, yellow } from "colors";
 
 // Belirli intent'ler ve partial'lar ile yeni bir client örneği oluştur
 const client = new Client({
@@ -18,22 +19,32 @@ const client = new Client({
 
 // ==== Çökme Koruyucusu // ==== //
 process.on("unhandledRejection", async (reason, promise) => {
-    console.error(`[ÇÖKME-KORUYUCU/unhandledRejection]: ${reason} ${promise}`);
+    console.error(
+        `${bold(
+            yellow("[ÇÖKME-KORUYUCU/unhandledRejection]:")
+        )} ${reason} ${promise}`
+    );
 });
 process.on("uncaughtException", async err => {
-    console.log(`[ÇÖKME-KORUYUCU/uncaughtException]: ${err}`);
+    console.log(
+        `${bold(yellow("[ÇÖKME-KORUYUCU/uncaughtException]:"))} ${err}`
+    );
 });
 process.on("uncaughtExceptionMonitor", async (err, origin) => {
-    console.log(`[ÇÖKME-KORUYUCU/uncaughtExceptionMonitor]: ${err} ${origin}`);
+    console.log(
+        `${bold(
+            yellow("[ÇÖKME-KORUYUCU/uncaughtExceptionMonitor]:")
+        )} ${err} ${origin}`
+    );
 });
 
 // ==== // İstemci Özellikleri // ====//
 // @ts-ignore
-client.commands = new Collection()
+client.commands = new Collection();
 // @ts-ignore
-client.slashCommands = new Collection()
+client.slashCommands = new Collection();
 // @ts-ignore
-client.aliases = new Collection()
+client.aliases = new Collection();
 
 // Botu .env dosyasındaki token ile giriş yap
 client.login(process.env.TOKEN);
