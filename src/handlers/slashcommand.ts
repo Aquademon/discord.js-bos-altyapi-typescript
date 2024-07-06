cfcd9532e289785da684cced2cf3c1020b59bb9c
Aquademon <98233916+Aquademon@users.noreply.github.com>
2024-07-06 19:44:11 +0300

    update src/handlers/slashcommand.ts

---
+++ src/handlers/slashcommand.ts
@@ -0,0 +1,17 @@
+import AsciiTable from "ascii-table";
+import { readdirSync } from "node:fs";
+import { join } from "node:path";
+import { blue, red, bold } from "colors";
+
+// Komutları ve durumlarını göstermek için ASCII tablosu oluştur
+const table = new AsciiTable()
+    .setHeading("Komut (/)", "Durum")
+    .setBorder("|", "=", "0", "0");
+
+export default client => {
+    const folders = readdirSync(join("src", "commands", "slash"), {
+        withFileTypes: true
+    })
+        .filter(dir => dir.isDirectory())
+        .map(dir => dir.name);
+};
