075492b2be22cc1ddc2d98511212234acbcce0b3
Aquademon <98233916+Aquademon@users.noreply.github.com>
2024-07-07 20:53:46 +0300

    update src/events/ready.ts

---
+++ src/events/ready.ts
@@ -0,0 +1,30 @@
+import { ActivityType } from "discord.js";
+import { client } from "../index";
+import colors from "colors";
+
+client.on("ready", client => {
+    console.log(colors.red(`${client.user.tag} İsimli Bot Şuan Aktif!`));
+
+    const activities = [
+        {
+            name: `Dark_Hunter & AquaDemon`,
+            type: ActivityType.Playing
+        }
+    ];
+
+    const status = ["dnd"];
+    let i = 0;
+    setInterval(() => {
+        if (i >= activities.length) i = 0;
+        client.user.setActivity(activities[i]);
+        i++;
+    }, 5000);
+
+    let s = 0;
+    setInterval(() => {
+        if (s >= activities.length) s = 0;
+        // @ts-ignore
+        client.user.setStatus(status[s]);
+        s++;
+    }, 30000);
+});
