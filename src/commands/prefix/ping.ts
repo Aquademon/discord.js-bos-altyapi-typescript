a148d3a43c7296e0f308ff16239f9ab29ba1171b
45t45t5454 <147265290+45t45t5454@users.noreply.github.com>
2024-07-11 16:44:59 +0300

    Delete src/commands directory

--- src/commands/prefix/ping.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    name: "deneme",
-    run: async function(client, message, args) {
-        await message.reply(`🏓 Pong ${client.ws.ping}ms`)
-    }
-};
--- src/commands/slash/ping.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  data: {
-    name: "ping",
-    description: "Botun Ping Gösterir",
-  },
-  run: async function(client,interaction) {
-await interaction.reply(`🏓 Pong ${client.ws.ping}ms`)
- }
-}
