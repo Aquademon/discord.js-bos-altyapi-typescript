8f3c591ead43104bd20d62562e95844507bcd695
Aquademon <98233916+Aquademon@users.noreply.github.com>
2024-12-07 20:55:27 +0300

    update src/events/ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -7,7 +7,7 @@ client.on("ready", client => {
 
     const activities = [
         {
-            name: `Dark_Hunter & AquaDemon`,
+            name: `by AquaDemon Development`,
             type: ActivityType.Playing
         }
     ];
